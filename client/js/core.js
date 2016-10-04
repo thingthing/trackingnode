9d3903c81cee11fc7a5cd0e10f707de4b4ab3b17
Thing-leoh Nicolas <nicolas.thingleoh@gmail.com>
2016-10-04 14:26:30 +0000

    Use aftership widget, works fine

--- client/js/core.js
+++ client/js/core.js
@@ -4,40 +4,28 @@ function mainController($scope, $http) {
     $scope.formData = {};
 
     // when landing on the page, get all todos and show them
-    $http.get('/couriers')
-        .success(function(data) {
-            $scope.couriers = data.data.couriers;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
+    // $http.get('/couriers')
+    //     .success(function(data) {
+    //         $scope.couriers = data.data.couriers;
+    //         console.log(data);
+    //     })
+    //     .error(function(data) {
+    //         console.log('Error: ' + data);
+    //     });
 
-    // when submitting the add form, send the text to the node API
-    $scope.send = function() {
-        $http.post('/send', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.main = data;
-                //if (data.meta.code != 200) $scope.main = data.meta.message;
-                //else $scope.main = data.data;
-                console.log("In angular data = " + data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
-
-    // delete a todo after checking it
-    $scope.deleteTodo = function(id) {
-        $http.delete('/api/todos/' + id)
-            .success(function(data) {
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
+    // // when submitting the add form, send the text to the node API
+    // $scope.send = function() {
+    //     $http.post('/send', $scope.formData)
+    //         .success(function(data) {
+    //             $scope.formData = {}; // clear the form so our user is ready to enter another
+    //             $scope.main = data;
+    //             //if (data.meta.code != 200) $scope.main = data.meta.message;
+    //             //else $scope.main = data.data;
+    //             console.log("In angular data = " + data);
+    //         })
+    //         .error(function(data) {
+    //             console.log('Error: ' + data);
+    //         });
+    // };
 
 }
